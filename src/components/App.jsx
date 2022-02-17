da1c6a98c33b14db261a161268510fbc913b548e
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2022-02-17 10:22:51 +0200

    Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
+import Profile from "./social-profile/Profile";
+import user from "./social-profile/user.json"
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
     </div>
   );
 };
