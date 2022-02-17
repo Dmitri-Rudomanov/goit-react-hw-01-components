dafd3437d9d5bc6e4cc173fa2ac10a59eea7c1da
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2022-02-17 15:39:01 +0200

    money-done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,11 @@ import Profile from "./social-profile/Profile";
 import user from "./social-profile/user.json"
 import data from "./statistics/data.json"
 import friends from "./friend-list/friends.json"
+import transactions from './transaction-history/transactions.json'
 import Statistics from "./statistics/Statistics";
 import FriendList from './friend-list/FriendList'
+import TransactionHistory from './transaction-history/TransactionHistory'
+
 
 export const App = () => {
   return (
@@ -20,6 +23,9 @@ export const App = () => {
       </div>
       <div>
         <FriendList friends={friends}/>
+      </div>
+      <div>
+        <TransactionHistory items={transactions}/>
       </div>
     </div>
     
