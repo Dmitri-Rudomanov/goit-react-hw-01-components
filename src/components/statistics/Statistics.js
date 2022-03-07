0a6b885f578b7cd51d7e08a6fb5c446c6b3df819
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2022-03-07 11:04:48 +0200

    fix-title

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -3,8 +3,8 @@ import s from "./Statistics.module.css"
 
 export default function Statistics({ title, stats }) { 
     return (
-        <section className={s.statistics}>
-    <h2 className={s.title}>{title}</h2>
+    <section className={s.statistics}>
+    {title&&<h2 className={s.title}>{title}</h2>}
 
     <ul className={s.statList}>
         {stats.map(stat => { 
